d6c17c1f622c8811d76face253b0831a70e96f34
Christopher Dancy <chris.dancy@decentlabs.io>
2022-03-12 09:21:34 -0500

    Added require statements to safeguard deploy

--- contracts/GuestListFactory.sol
+++ contracts/GuestListFactory.sol
@@ -9,11 +9,20 @@ contract GuestListFactory is IGuestListFactory{
     function createGuestList(
         address guestlistImpl_,
         address wrapper_,
+        address newOwner_,
         uint256 userCap_,
         uint256 totalCap_,
-        bytes32 guestRoot_,
-        address newOwner_
+        bytes32 guestRoot_
     ) external returns (ITestVipCappedGuestListBbtcUpgradeable clone) {
+        // SafeGuard Parmas
+        if(guestlistImpl_ == address(0)) revert AddressZero();
+        if(wrapper_ == address(0)) revert AddressZero();
+        if(newOwner_ == address(0)) revert AddressZero();
+        if(userCap_ == 0) revert UintZero();
+        if(totalCap_ == 0) revert UintZero();
+        if(guestRoot_ == bytes32(0)) revert Bytes32Zero();
+
+        // Deploy Clone
         clone = ITestVipCappedGuestListBbtcUpgradeable(
                 ClonesUpgradeable.clone(guestlistImpl_)
             );
@@ -25,6 +34,6 @@ contract GuestListFactory is IGuestListFactory{
         clone.setGuestRoot(guestRoot_);
         clone.transferOwnership(newOwner_);
 
-        emit GuestListCreated(guestlistImpl_, wrapper_, userCap_, totalCap_, guestRoot_, newOwner_);
+        emit GuestListCreated(guestlistImpl_, wrapper_, newOwner_, userCap_, totalCap_, guestRoot_);
     }
 }
--- interfaces/IGuestListFactory.sol
+++ interfaces/IGuestListFactory.sol
@@ -4,20 +4,24 @@ pragma solidity ^0.8.0;
 import "../interfaces/ITestVipCappedGuestListBbtcUpgradeable.sol.sol";
 
 interface IGuestListFactory {
+    error AddressZero();
+    error UintZero();
+    error Bytes32Zero();
+    
     event GuestListCreated(address guestlistImpl_,
         address wrapper_,
+        address newOwner_,
         uint256 userCap_,
         uint256 totalCap_,
-        bytes32 guestRoot_,
-        address newOwner_
+        bytes32 guestRoot_
     );
 
     function createGuestList(
         address guestlistImpl_,
         address wrapper_,
+        address newOwner_,
         uint256 userCap_,
         uint256 totalCap_,
-        bytes32 guestRoot_,
-        address newOwner_
+        bytes32 guestRoot_
     ) external returns (ITestVipCappedGuestListBbtcUpgradeable clone);
 }
