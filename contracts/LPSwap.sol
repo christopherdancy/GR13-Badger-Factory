dae0a66cce36a848f127595c2ec83662b28850f7
Rémi Colin <98749896+remicolin@users.noreply.github.com>
2022-03-22 11:04:31 +0100

    Update LPSwap.sol

--- contracts/LPSwap.sol
+++ contracts/LPSwap.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./utils/HomoraMath.sol";
 import "../interfaces/ILPSwap.sol";
+import "../interfaces/IUniswapV2Pair.sol";
 
 contract LPSwap is ILPSwap {
     using SafeMath for uint256;
     using HomoraMath for uint256;
+    uint256 public reserve0;
 
     function findLPSwap(
         IUniswapV2Pair pair,
@@ -19,7 +21,9 @@ contract LPSwap is ILPSwap {
         address token0 = IUniswapV2Pair(pair).token0();
         address token1 = IUniswapV2Pair(pair).token1();
         uint256 totalSupply = IUniswapV2Pair(pair).totalSupply();
-        (uint256 r0, uint256 r1, ) = IUniswapV2Pair(pair).getReserves();
+        (uint256 r0, uint256 r1, uint256 timestamp) = IUniswapV2Pair(pair)
+            .getReserves();
+        reserve0 = r0;
         // Calulcate K
         uint256 sqrtK = HomoraMath.sqrt(r0.mul(r1)).fdiv(totalSupply); // in 2**112
         // Reserves value in ETH
@@ -40,18 +44,23 @@ contract LPSwap is ILPSwap {
         lpUSD = _checkUni(weth, usdDenomToken, LPWeth, router);
     }
 
-    function _checkUni(address tokenIn, address weth, uint256 amountIn, IUniswapRouterV2 router) internal returns(uint px) {
-            // Check uni (Can Revert)
-            address[] memory path = new address[](2);
-            path[0] = address(tokenIn);
-            path[1] = address(weth);
+    function _checkUni(
+        address tokenIn,
+        address weth,
+        uint256 amountIn,
+        IUniswapRouterV2 router
+    ) internal returns (uint256 px) {
+        // Check uni (Can Revert)
+        address[] memory path = new address[](2);
+        path[0] = address(tokenIn);
+        path[1] = address(weth);
 
-            try router.getAmountsOut(amountIn, path) returns (
-                uint256[] memory uniAmounts
-            ) {
-                px = uniAmounts[uniAmounts.length - 1]; // Last one is the outToken
-            } catch (bytes memory) {
-                // We ignore as it means it's zero
-            }
+        try router.getAmountsOut(amountIn, path) returns (
+            uint256[] memory uniAmounts
+        ) {
+            px = uniAmounts[uniAmounts.length - 1]; // Last one is the outToken
+        } catch (bytes memory) {
+            // We ignore as it means it's zero
+        }
     }
 }
